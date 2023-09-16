313f097b2d53f4b1179f1df7afb31bf244f99fb1
Hanna <gkssk4763@naver.com>
2023-09-17 02:01:11 +0900

    Time: 1612 ms (17.39%), Space: 34.1 MB (14.33%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,43 +1,14 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        table = {}
-        count = {}
+        num_dict = {key:True for key in nums}
+        ans = 0
         for n in nums:
-            if n in table:
-                continue
-            if n-1 in table and n+1 in table:
-                prev = table[n-1]
-                while prev != table[prev]:
-                    prev = table[prev]
-                next = table[n+1]
-                while next != table[next]:
-                    next = table[next]
-
-                count[prev] += (1 + count[next])
-                table[next] = prev
-                table[n] = prev
-            
-            elif n-1 in table:
-                prev = table[n-1]
-                while prev != table[prev]:
-                    prev = table[prev]
-
-                count[prev] += 1
-                table[n] = prev
-            
-            elif n+1 in table:
-                next = table[n+1]
-                while next != table[next]:
-                    next = table[next]
-
-                count[n] = (1 + count[next])
-                table[next] = n
-                table[n] = n
-            
-            else:
-                table[n] = n
-                count[n] = 1
-
-        return max(count.values())       
+            if n-1 not in num_dict:
+                cnt = 0
+                cur = n
+                while cur in num_dict:
+                    cnt += 1
+                    cur += 1
+                ans = max(ans, cnt)
+        
+        return ans
