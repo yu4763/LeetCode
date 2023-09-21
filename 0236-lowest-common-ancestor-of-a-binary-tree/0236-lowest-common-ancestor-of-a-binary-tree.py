182604bf244b8d599c0b4869442803bfb76c7567
Hanna <gkssk4763@naver.com>
2023-09-21 23:59:41 +0900

    Time: 67 ms (56.75%), Space: 28.5 MB (85.39%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,30 +8,16 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        res = []
-        self.dfs(root, p, q, res)
-        return res[0]
+        if root == None or root == p or root == q:
+           return root
+           
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-    def dfs(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode', res: list):
-
-        if root == None:
-            return False, False
-            
-        found_p = False
-        found_q = False
-        if root == p:
-            found_p = True
-        if root == q:
-            found_q = True
-        
-        
-        p1, q1 = self.dfs(root.left, p, q, res)
-        p2, q2 = self.dfs(root.right, p, q, res)
-        
-        result_p = found_p or p1 or p2
-        result_q = found_q or q1 or q2
-
-        if result_p and result_q:
-            res.append(root)
-        
-        return (result_p, result_q)
+        if left and right:
+            return root
+        elif left:
+            return left
+        elif right:
+            return right
+        return None
