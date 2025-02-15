1c8a6db5014065e35a32da5990df60a19ce83a04
Hanna <gkssk4763@naver.com>
2025-02-15 12:59:45 +0900

    Time: 0 ms (100%), Space: 12.8 MB (12.13%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -5,12 +5,15 @@ class Solution(object):
         :rtype: int
         """
         N = len(citations)
-        count = [0] * 1001
+        count = [0] * (N+1)
         for c in citations:
-            count[c] += 1
+            if c >= N:
+                count[N] += 1
+            else:
+                count[c] += 1
 
         total = 0
-        for i in range(1000, -1, -1):
+        for i in range(N, -1, -1):
             total += count[i]
             if total >= i:
                 return i
